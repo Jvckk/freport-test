d2871072869ffb7d8524e5a7542e12cc933707db
Jvckk <cjaekwang@gmail.com>
2025-05-29 22:25:54 +0200

    initialized firebase

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZJ1xRhr4jbhAe9Cg87otOsatTRVoJqGE",
+  authDomain: "freport-test.firebaseapp.com",
+  projectId: "freport-test",
+  storageBucket: "freport-test.firebasestorage.app",
+  messagingSenderId: "620724602040",
+  appId: "1:620724602040:web:a670fca83b0e2e45f7204d",
+  measurementId: "G-3ZDXSH3DNZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
